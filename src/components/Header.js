65e21cfb45402ea31705f87cfdb9b9c642084844
Andy checo <savedcity@Andys-MacBook-Air.local>
2021-08-03 07:39:35 -0500

    remove and css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Today from './Today'
 import Tomorrow from './Tomorrow'
 
-const Header = (props) => {
+const Header = () => {
+
   return (
     <Router>
       <nav>
         <Link className='nav-today' to='/today'>TODAY</Link>
         <Link className='nav-tomorrow' to='/tomorrow'>TOMORROW</Link>
       </nav>
-      <Route path='/today' component={Today}/>
+      <Route path='/today'>
+        <Today/>
+      </Route>
       <Route path='/tomorrow' component={Tomorrow}/>
     </Router>
   )
--- src/components/Today.js
+++ src/components/Today.js
@@ -1,22 +1,98 @@
 import React, {useState} from 'react';
+import { AiFillDelete } from 'react-icons/ai';
+// import { AiFillEdit } from 'react-icons/ai';
 
-export default function Today() {
-    const [todos, setTodos] = useState([
-        {id: 1, name: 'shower'},
-        {id: 2, name: 'eat'}
-    ])
+
+export default function Today(props) {
+    const [todo, setTodo] = useState('');
+    const [todoList, setTodoList] = useState([]);
+    // const [edit, setEdit] = useState({
+    //     id: null,
+    //     value: ''
+    // })
+    
+    let addTodo = task => {
+        if (!task.text || /^\s*$/.test(task.text)) {
+            return;
+        }
+        const newTodo = [task, ...todoList]
+        setTodoList(newTodo)
+        console.log( ...todoList)
+    }
+
+    let handleTodo = (e) => {
+        setTodo(e.target.value);
+    }
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        setTodo('');
+        addTodo({
+            id: ~~(Math.random() * 1000000),
+            text: todo,
+        })
+    }
+
+    let completeTodo = id => {
+        let updatedTodos = todoList.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        });
+        setTodoList(updatedTodos)
+        console.log(updatedTodos)
+    }
+
+    let removeTodo = id => {
+        const removeArr = [...todoList].filter(todo => todo.id !== id)
+
+        setTodoList(removeArr)
+    }
+
+    // let updateTodo = (todoId, newValue) => {
+    //     if (!newValue.text || /^\s*$/.test(newValue.text)) {
+    //         return;
+    //     }
+
+    //     setTodoList(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+    // }
+
+    // let submitUpdate = value => {
+    //     updateTodo(edit.id, value)
+    //     setEdit({
+    //         id: null,
+    //         value: ''
+    //     })
+    // }
+    
     return (
         <div>
-            <form className='add-form center'>
-                <label htmlFor='name'>ADD TO DO</label>
-                <input className='name-input' type='text' id='name' placeholder='NAME'/>
-                <input className='submit-input' type='submit' value='SUBMIT'/>
+            <form onSubmit={handleSubmit} className='add-form center'>
+
+                <input autoFocus value={todo} name='text' onChange={handleTodo} className='name-input' type='text' id='name' placeholder='Task...'/>
+
+                <input onClick={props.addTodo} className='submit-input' type='submit' value='Add'/>
+
             </form>
 
-            <div className='center column'>
-                <h2>TODAY'S TO-DOS:</h2>
-                {todos.map((item) => {
-                    <h3>{item.name}</h3>
+            <div>
+                <h2 className='center column todo-title'>To-Do List:</h2>
+                {todoList.map((todo, index) => {
+                    return (
+                    <div key={index}  className={todo.isComplete ? 'todo-container center completed' : 'todo-container center'}>
+                        
+                        <div onClick={() => completeTodo(todo.id)}>
+
+                            <h1 className='todo-name' >{todo.text} </h1>
+
+                            {/* <AiFillEdit onClick={() => setEdit( {id: todo.id, value: todo.text} )} className='edit-icon'/> */}
+
+                        </div>
+
+                        <AiFillDelete onClick={() => removeTodo(todo.id)} className='delete-icon'/>
+                    </div>
+                    )
                 })}
             </div>
         </div>
